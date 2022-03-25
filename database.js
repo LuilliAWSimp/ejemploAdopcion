cc656253817294a4686b8ed9736d724c923f463b
LuilliAWSimp <83787444+LuilliAWSimp@users.noreply.github.com>
2022-03-24 18:14:44 -0600

    Update database.js

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ let config = {
 	host: "us-cdbr-east-05.cleardb.net",
 	user: "bb921597f3d5a4",
 	password: "c2130b4a",
-	port: 3306,
+	port: 3307,
 	database: "heroku_b762dde87862340",
 	secret_key:"ArribaElAmerica"
 };
